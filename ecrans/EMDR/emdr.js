f78f26b04690732439167f8e6f133d167c200c70
BEDDAR <beddarrebiha@gmail.com>
2023-12-27 23:25:47 +0100

    EMDR

--- ecrans/EMDR/emdr.js
+++ ecrans/EMDR/emdr.js
@@ -1,12 +1,61 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { View, TouchableOpacity, Animated, Text,Dimensions ,Easing} from 'react-native';
+import emdrStyle from './emdrStyle';
 
-const EMDR=()=>{
-    return (
-        <View>
-            <Text>EMDR</Text>
-        </View>
-    )
+var { width} = Dimensions.get('window');
+export default class EMDR extends Component {
+    
+    state = {
+        xValue: new Animated.Value(0),
+    }
+      animer = () => {
+        Animated.timing(this.state.xValue, {
+          toValue: width - 100,
+          duration: 1000,
+          easing: Easing.linear,      
+          // easing: Easing.back(),     
+          // easing: Easing.cubic,  
+          useNativeDriver:false    
+        }).start(() => {
+          //Call after finish this animation !
+          Animated.timing(this.state.xValue, {
+            toValue: 0,
+            duration: 1000,
+            easing: Easing.linear,
+            // easing: Easing.back(),     
+            // delay: 1000,//run after 1 seconds
+            useNativeDriver:false
+          }).start(() => {
+            t= setInterval(()=>this.animer(),20000)
+            clearInterval(t)
+            
+          });
+        });
+      }
+    render() {
+
+        const trans = {
+
+            transform: [
+                { translateY: this.state.position }
+            ]
+        }
+        
+
+        return (
+            <>
+                <Animated.View  style={[emdrStyle.point,{ left: this.state.xValue } ]}>
+
+                </Animated.View>
+                <View >
+
+                    <TouchableOpacity onPress={this.animer}>
+                        <Text>Commencer</Text>
+                    </TouchableOpacity>
+
+                </View>
+            </>
+        );
+    }
 }
 
-export default EMDR
---
+++ ecrans/EMDR/emdrStyle.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const emdrStyle = StyleSheet.create({
+
+    point:{
+        height:80,
+        width:80,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'red',
+        borderRadius:50
+    }
+})
+
+export default emdrStyle
