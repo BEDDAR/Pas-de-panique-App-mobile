dc81775b7145feb81f34d9975db4b658d8630285
BEDDAR <beddarrebiha@gmail.com>
2023-12-28 10:13:35 +0100

    EMDR

--- ecrans/EMDR/emdr.js
+++ ecrans/EMDR/emdr.js
@@ -1,60 +1,52 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, Animated, Text,Dimensions ,Easing} from 'react-native';
+import { View, Button, Animated, Text, Dimensions, Easing, SafeAreaView, TouchableOpacity } from 'react-native';
 import emdrStyle from './emdrStyle';
 
-var { width} = Dimensions.get('window');
+var { width } = Dimensions.get('window');
 export default class EMDR extends Component {
-    
+
     state = {
         xValue: new Animated.Value(0),
     }
-      animer = () => {
+    animer = () => {
         Animated.timing(this.state.xValue, {
-          toValue: width - 100,
-          duration: 1000,
-          easing: Easing.linear,      
-          // easing: Easing.back(),     
-          // easing: Easing.cubic,  
-          useNativeDriver:false    
-        }).start(() => {
-          //Call after finish this animation !
-          Animated.timing(this.state.xValue, {
-            toValue: 0,
-            duration: 1000,
+            toValue: width-60,
+            duration: 3000,
             easing: Easing.linear,
             // easing: Easing.back(),     
-            // delay: 1000,//run after 1 seconds
-            useNativeDriver:false
-          }).start(() => {
-            t= setInterval(()=>this.animer(),20000)
-            clearInterval(t)
-            
-          });
+            // easing: Easing.cubic,  
+            useNativeDriver: false
+        }).start(() => {
+            //Call after finish this animation !
+            Animated.timing(this.state.xValue, {
+                toValue: 0,
+                duration: 3000,
+                easing: Easing.linear,
+                // easing: Easing.back(),     
+                // delay: 2000,//run after 2 seconds
+                useNativeDriver: false
+            }).start(() => {
+                this.animer()
+            });
         });
-      }
+    }
     render() {
-
-        const trans = {
-
-            transform: [
-                { translateY: this.state.position }
-            ]
-        }
-        
-
         return (
-            <>
-                <Animated.View  style={[emdrStyle.point,{ left: this.state.xValue } ]}>
-
+            <SafeAreaView style={emdrStyle.contenaire}>
+                <Animated.View style={[emdrStyle.point, { left: this.state.xValue }]}>
                 </Animated.View>
-                <View >
-
-                    <TouchableOpacity onPress={this.animer}>
-                        <Text>Commencer</Text>
+                <View style={{ alignItems: 'center' }}>
+                    <TouchableOpacity style={emdrStyle.bouton} onPress={this.animer}>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white' }}>Commencer</Text>
                     </TouchableOpacity>
-
+                    <Text style={{ textAlign: 'center', fontSize: 15, fontWeight: 'bold', color: 'black' }}>
+                        Sans bouger votre tête, suivre les mouvement de la boule.{"\n"}
+                        Pensez en même temps à la situation qui génère des émotions douloureuses.{"\n"}
+                        Après cette Séance ces émotions difficiles seront fortement atténuées.{"\n"} {"\n"}
+                        N'hésitez-pas à répéter cet exercice pour conforeter le résultat.
+                    </Text>
                 </View>
-            </>
+            </SafeAreaView>
         );
     }
 }
--- ecrans/EMDR/emdrStyle.js
+++ ecrans/EMDR/emdrStyle.js
@@ -2,13 +2,25 @@ import { StyleSheet } from "react-native";
 
 const emdrStyle = StyleSheet.create({
 
-    point:{
-        height:80,
-        width:80,
+    point: {
+        height: 80,
+        width: 80,
+        backgroundColor: 'red',
+        borderRadius: 50,
+    },
+    contenaire: {
+        flex:1,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    bouton: {
+        backgroundColor:"#087919",
+        width:150,
+        height:60,
         alignItems:'center',
         justifyContent:'center',
-        backgroundColor:'red',
-        borderRadius:50
+        borderRadius:10,
+        marginBottom:20
     }
 })
 
